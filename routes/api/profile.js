647dd149fba799819689095e295b59136ec21e5d
Suman Kumar <sumancsnit@gmail.com>
2019-10-29 23:21:59 +0530

    new changes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -89,7 +89,7 @@ router.post('/', [
 
       if (profile) {
         // Update
-        profile = await Profile.findByIdAndUpdate(
+        profile = await Profile.findOneAndUpdate(
           { user: req.user.id },
           { $set: profileFields },
           { new: true }
