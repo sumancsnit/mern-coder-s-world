8d4b12953adab86562e0482ba273ead827b7a5a5
Suman Kumar <sumancsnit@gmail.com>
2019-11-01 23:09:29 +0530

    test

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('API Running'));
 
+// test 
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
